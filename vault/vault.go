8ad0c3b0a6104e142118a89b01d42f1284365ab1
Vincent Desjardins <vdesjardins@gmail.com>
2017-09-26 23:06:23 -0400

    better handling of Vault status error code

--- vault/vault.go
+++ vault/vault.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -46,42 +47,57 @@ type loginResponse struct {
 }
 
 func (client Client) refreshToken() (string, error) {
-	var message loginResponse
-	var token string
-
 	loginInfo := loginRequest{client.RoleId, client.SecretId}
 
 	loginPayload, err := json.Marshal(loginInfo)
 	if err != nil {
-		return token, err
+		return "", fmt.Errorf("Error marshalling Vault request: %v", err)
 	}
 
 	loginPayloadReader := bytes.NewReader(loginPayload)
 	url := client.BaseUrl.ResolveReference(&client.LoginPath).String()
 	req, err := http.NewRequest(http.MethodPost, url, loginPayloadReader)
 	if err != nil {
-		return token, err
+		return "", fmt.Errorf("Error creating Vault request: %v", err)
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return token, err
+		return "", fmt.Errorf("Error calling Vault: %v", err)
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return token, err
-	}
-	if err := json.Unmarshal(body, &message); err != nil {
-		return token, err
+	if resp.StatusCode != 200 {
+		errors := ""
+		message, err := client.readJSONResponse(resp.Body)
+		if err == nil {
+			errors = fmt.Sprintf("errors: %v", message.Errors)
+		}
+
+		return "", fmt.Errorf("Error: vault auth status: %d %v", resp.StatusCode, errors)
 	}
 
-	if resp.StatusCode != 200 {
-		return token, fmt.Errorf("Error: vault auth status: %d errors: %v", resp.StatusCode, message.Errors)
+	message, err := client.readJSONResponse(resp.Body)
+	if err != nil {
+		return "", err
 	}
 
 	return message.Auth.ClientToken, nil
+}
+
+func (client Client) readJSONResponse(body io.ReadCloser) (loginResponse, error) {
+	var message loginResponse
+
+	b, err := ioutil.ReadAll(body)
+	if err != nil {
+		return message, fmt.Errorf("Error reading Vault response: %v", err)
+	}
+
+	if err := json.Unmarshal(b, &message); err != nil {
+		return message, fmt.Errorf("Error unmarshalling Vault response: %v", err)
+	}
+
+	return message, nil
 }
 
 func (client Client) FetchNewCertificate(certReq CertRequest) (CertResponse, error) {
@@ -89,40 +105,42 @@ func (client Client) FetchNewCertificate(certReq CertRequest) (CertResponse, err
 
 	vaultToken, err := client.refreshToken()
 	if err != nil {
-		return message, err
+		return message, fmt.Errorf("Error refreshing Vault token: %v", err)
 	}
 
 	return client.fetchNewCertificate(certReq, vaultToken)
 }
+
 func (client Client) fetchNewCertificate(certReq CertRequest, vaultToken string) (CertResponse, error) {
 	var message CertResponse
 
 	certPayload, err := json.Marshal(certReq)
 	if err != nil {
-		return message, err
+		return message, fmt.Errorf("Error marshalling Vault request: %v", err)
 	}
 
 	certPayloadReader := bytes.NewReader(certPayload)
 	url := client.BaseUrl.ResolveReference(&client.CertPath).String()
+
 	req, err := http.NewRequest(http.MethodPost, url, certPayloadReader)
 	if err != nil {
-		return message, err
+		return message, fmt.Errorf("Error creating request: %v", err)
 	}
 
 	req.Header.Add("X-Vault-Token", vaultToken)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return message, err
+		return message, fmt.Errorf("Error calling Vault: %v", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return message, err
+		return message, fmt.Errorf("Error reading Vault response: %v", err)
 	}
 	if err := json.Unmarshal(body, &message); err != nil {
-		return message, err
+		return message, fmt.Errorf("Error unmarshalling Vault response: %v", err)
 	}
 
 	if resp.StatusCode != 200 {
